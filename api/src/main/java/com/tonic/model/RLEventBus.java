fd00bdde1a1948441b3bfcd03401a921af528683
TonicBox <gsec.tonicbox@protonmail.com>
2025-08-14 19:37:03 -0500

    Remapper and RuneLite Reflection module upgrades

---
+++ api/src/main/java/com/tonic/model/RLEventBus.java
@@ -0,0 +1,49 @@
+package com.tonic.model;
+
+import com.tonic.Logger;
+import com.tonic.util.ReflectUtil;
+
+public class RLEventBus
+{
+    private final Object eventBus;
+
+    public RLEventBus(Guice injector) {
+        this.eventBus = injector.getBinding("net.runelite.client.eventbus.EventBus");
+    }
+
+    public void post(Object event) {
+        try
+        {
+            Class<?> eventClass = event.getClass();
+            ReflectUtil.getMethod(eventBus, "post", new Class[]{eventClass}, new Object[]{event});
+        }
+        catch (Exception e)
+        {
+            Logger.error("Failed to post event: " + event.getClass().getName());
+        }
+    }
+
+    public void register(Object listener) {
+        try
+        {
+            Class<?> listenerClass = listener.getClass();
+            ReflectUtil.getMethod(eventBus, "register", new Class[]{listenerClass}, new Object[]{listener});
+        }
+        catch (Exception e)
+        {
+            Logger.error("Failed to register listener: " + listener.getClass().getName());
+        }
+    }
+
+    public void unregister(Object listener) {
+        try
+        {
+            Class<?> listenerClass = listener.getClass();
+            ReflectUtil.getMethod(eventBus, "unregister", new Class[]{listenerClass}, new Object[]{listener});
+        }
+        catch (Exception e)
+        {
+            Logger.error("Failed to unregister listener: " + listener.getClass().getName());
+        }
+    }
+}
---
+++ api/src/main/java/com/tonic/util/ReflectBuilder.java
@@ -0,0 +1,6 @@
+package com.tonic.util;
+
+public class ReflectBuilder
+{
+
+}
