85abf822e1819388fafeda30ea33f343e4edbe45
TonicBox <gsec.tonicbox@protonmail.com>
2025-08-19 16:16:41 -0500

    Tweak to handling rl login identification

--- src/main/java/com/tonic/mixins/TClientMixin.java
+++ src/main/java/com/tonic/mixins/TClientMixin.java
@@ -3,6 +3,8 @@ package com.tonic.mixins;
 import com.tonic.api.*;
 import com.tonic.injector.annotations.*;
 
+import java.util.Random;
+
 @Mixin("Client")
 public abstract class TClientMixin implements TClient
 {
@@ -29,18 +31,23 @@ public abstract class TClientMixin implements TClient
     @Shadow("packedCallStack2")
     private static String packedClassStack2;
 
+    @Inject
+    private static String[] generatedPackedPairs;
+
     @MethodOverride("callStackPacker1")
     @SkipPoison
     public static void callStackPacker1()
     {
-        packedClassStack1 = "555+";
+        generateNumberPair();
+        packedClassStack1 = generatedPackedPairs[0];
     }
 
     @MethodOverride("callStackPacker2")
     @SkipPoison
     public static void callStackPacker2()
     {
-        packedClassStack2 = "638+";
+        generateNumberPair();
+        packedClassStack2 = generatedPackedPairs[1];
     }
 
     @MethodOverride("callStackCheck")
@@ -52,5 +59,32 @@ public abstract class TClientMixin implements TClient
                 "nrc.RuneLite276main\n" +
                 "nrl.ReflectionLa+64lambda$launc+\n" +
                 "jl.ThreadUnknown +";
+    }
+
+    @Inject
+    public static void generateNumberPair() {
+        if(generatedPackedPairs != null) {
+            return;
+        }
+        Random rand = new Random();
+
+        int firstNum = 137 + rand.nextInt(650);
+
+        int difference;
+        double prob = rand.nextDouble();
+        if (prob < 0.7) {
+            difference = 90;
+        } else if (prob < 0.85) {
+            difference = 85 + rand.nextInt(7);
+        } else {
+            difference = rand.nextBoolean() ? 85 : 107;
+        }
+
+        int secondNum = firstNum + difference;
+
+        generatedPackedPairs = new String[] {
+                firstNum + "+",
+                secondNum + "+"
+        };
     }
 }
