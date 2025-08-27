2d420b93b6b062e2f4075dd23c7b4c56da165268
TonicBox <gsec.tonicbox@protonmail.com>
2025-08-27 17:05:01 -0500

    Cleanup

--- src/main/java/com/tonic/injector/util/expreditor/impls/ModifyResourceLoading.java
+++ src/main/java/com/tonic/injector/util/expreditor/impls/ModifyResourceLoading.java
@@ -2,21 +2,16 @@ package com.tonic.injector.util.expreditor.impls;
 
 import com.tonic.injector.util.BytecodeBuilder;
 import com.tonic.injector.util.expreditor.ExprEditor;
-import com.tonic.injector.util.expreditor.FieldAccess;
 import com.tonic.injector.util.expreditor.LiteralValue;
-import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.*;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class ModifyResourceLoading extends ExprEditor
 {
     private final List<AbstractInsnNode> toRemove = new ArrayList<>();
-    private final ExprEditor editor = new ExprEditor() {
+    private final ExprEditor subEditor = new ExprEditor() {
         private MethodNode method;
-        private String fieldName = null;
-        private String fieldOwner = null;
 
         @Override
         public void edit(LiteralValue literal) {
@@ -37,7 +32,6 @@ public class ModifyResourceLoading extends ExprEditor
 
         private void patchSoundLoad(LiteralValue literal)
         {
-            System.out.println("Patching load of: " + literal.getStringValue());
             InsnList insns = BytecodeBuilder.create()
                     .pop2()
                     .pop()
@@ -48,25 +42,6 @@ public class ModifyResourceLoading extends ExprEditor
             toRemove.add(methodCall);
             method.instructions.insert(target, insns);
         }
-
-        private void patchPcmLoad(LiteralValue literal)
-        {
-            InsnList insns = BytecodeBuilder.create()
-                    .pop2()
-                    .pop2()
-                    .pushNull()
-                    .build();
-
-            AbstractInsnNode target = literal.getLiteralInstruction().getNext();
-            if(!(target.getNext() instanceof MethodInsnNode))
-                return;
-            MethodInsnNode methodCall = (MethodInsnNode) target.getNext();
-            FieldInsnNode field = (FieldInsnNode) methodCall.getNext();
-            fieldName = field.name;
-            fieldOwner = field.owner;
-            toRemove.add(methodCall);
-            method.instructions.insert(target, insns);
-        }
     };
 
     @Override
@@ -74,9 +49,7 @@ public class ModifyResourceLoading extends ExprEditor
         if (!literal.isString() || !literal.getStringValue().equals("Sound FX"))
             return;
 
-        System.out.println("Patching Sound Load: " + literal.getClassNode().name + "." + literal.getMethod().name);
-
-        editor.instrument(literal.getClassNode(), literal.getMethod());
+        subEditor.instrument(literal.getClassNode(), literal.getMethod());
         for(AbstractInsnNode insn : toRemove)
         {
             literal.getMethod().instructions.remove(insn);
