c91f17f26035238e2145be596f98211dad52d6d4
TonicBox <gsec.tonicbox@protonmail.com>
2025-10-13 14:36:21 -0500

    Changes to headless mode mixin stuff, mixin for handling client errors, few aditional mappings

--- src/main/java/com/tonic/mixins/TGameEngineMixin.java
+++ src/main/java/com/tonic/mixins/TGameEngineMixin.java
@@ -2,21 +2,45 @@ package com.tonic.mixins;
 
 import com.tonic.Static;
 import com.tonic.api.TGameEngine;
-import com.tonic.injector.annotations.Disable;
-import com.tonic.injector.annotations.Mixin;
+import com.tonic.injector.annotations.*;
+import org.slf4j.Logger;
 
 @Mixin("GameEngine")
 public class TGameEngineMixin implements TGameEngine
 {
-    @Disable("graphicsTick")
-    public static boolean onGraphicsTick()
+    @Shadow("logger")
+    public static Logger logger;
+
+    @Shadow("graphicsGuard")
+    public static boolean graphicsGuard;
+
+    @FieldHook("graphicsGuard")
+    public static boolean onGuardSet(boolean bool)
     {
-        return !Static.isHeadless();
+        if(Static.isHeadless())
+        {
+            graphicsGuard = false;
+            return false;
+        }
+        return true;
     }
 
-//    @Disable("post")
-//    public static boolean post()
-//    {
-//        return !Static.isHeadless();
-//    }
+    @Disable("processError")
+    public static boolean processError(String message, Throwable error)
+    {
+        Throwable var3 = error;
+        if(error instanceof Iterable && "".equals(error.getMessage())) {
+            var3 = error.getCause();
+        }
+
+        if(message == null) {
+            logger.error("Client error", var3);
+            com.tonic.Logger.warn("Client error: " + var3.getMessage());
+        }
+        else {
+            logger.error("Client error: {}", message, var3);
+            com.tonic.Logger.warn("Client error: " + message);
+        }
+        return false;
+    }
 }
