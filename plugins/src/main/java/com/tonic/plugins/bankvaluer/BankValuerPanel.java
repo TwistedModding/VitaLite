c4561af138f1526787ac8fe57bf3b7c82e5f5bb7
TonicBox <gsec.tonicbox@protonmail.com>
2025-10-14 00:54:25 -0500

    Tweaks to Bank Valuer panel

--- plugins/src/main/java/com/tonic/plugins/bankvaluer/BankValuerPanel.java
+++ plugins/src/main/java/com/tonic/plugins/bankvaluer/BankValuerPanel.java
@@ -1,6 +1,7 @@
 package com.tonic.plugins.bankvaluer;
 
 import com.tonic.model.ui.components.FancyCard;
+import com.tonic.services.BankCache;
 import net.runelite.client.ui.ColorScheme;
 import net.runelite.client.ui.FontManager;
 import net.runelite.client.ui.PluginPanel;
@@ -19,23 +20,16 @@ public class BankValuerPanel extends PluginPanel
 	{
 		setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 		setBackground(ColorScheme.DARK_GRAY_COLOR);
-		setLayout(new GridBagLayout());
-
-		GridBagConstraints c = new GridBagConstraints();
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.gridx = 0;
-		c.gridy = 0;
-		c.weightx = 1;
-		c.insets = new Insets(0, 0, 10, 0);
+		setLayout(new BorderLayout());
 
 		FancyCard card = new FancyCard("Bank Valuer", "Top 10 most valuable items in your bank");
-		add(card, c);
-		c.gridy++;
+		add(card, BorderLayout.NORTH);
 
 		itemContainer = new JPanel();
 		itemContainer.setLayout(new BoxLayout(itemContainer, BoxLayout.Y_AXIS));
 		itemContainer.setBackground(ColorScheme.DARK_GRAY_COLOR);
-		add(itemContainer, c);
+		itemContainer.setBorder(new EmptyBorder(10, 0, 0, 0));
+		add(itemContainer, BorderLayout.CENTER);
 	}
 
 	public void reset()
@@ -65,12 +59,15 @@ public class BankValuerPanel extends PluginPanel
 			}
 			else
 			{
+				final int[] rank = {1};
 				topItems.entrySet().stream()
 					.sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
 					.forEach(entry -> {
-						JPanel itemRow = createItemRow(entry.getKey(), entry.getValue());
+						int quantity = BankCache.cachedBankCount(entry.getKey());
+						JPanel itemRow = createItemRow(rank[0], entry.getKey(), quantity, entry.getValue());
 						itemContainer.add(itemRow);
 						itemContainer.add(Box.createRigidArea(new Dimension(0, 5)));
+						rank[0]++;
 					});
 			}
 
@@ -79,17 +76,20 @@ public class BankValuerPanel extends PluginPanel
 		});
 	}
 
-	private JPanel createItemRow(int itemId, long value)
+	private JPanel createItemRow(int rank, int itemId, int quantity, long value)
 	{
+		JPanel wrapper = new JPanel(new BorderLayout(5, 0));
+		wrapper.setBackground(ColorScheme.DARK_GRAY_COLOR);
+		wrapper.setMaximumSize(new Dimension(Integer.MAX_VALUE, 60));
+
 		JPanel row = new JPanel(new BorderLayout(10, 0));
 		row.setBackground(ColorScheme.DARKER_GRAY_COLOR);
 		row.setBorder(new EmptyBorder(8, 10, 8, 10));
-		row.setMaximumSize(new Dimension(Integer.MAX_VALUE, 60));
 
 		JLabel iconLabel = new JLabel();
 		iconLabel.setVerticalAlignment(SwingConstants.CENTER);
 		iconLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		BankValuerUtils.getItemImage(iconLabel, itemId, 1);
+		BankValuerUtils.getItemImage(iconLabel, itemId, quantity);
 		row.add(iconLabel, BorderLayout.WEST);
 
 		JPanel centerPanel = new JPanel();
@@ -98,12 +98,12 @@ public class BankValuerPanel extends PluginPanel
 
 		JLabel nameLabel = new JLabel(BankValuerUtils.getName(itemId));
 		nameLabel.setForeground(ColorScheme.LIGHT_GRAY_COLOR);
-		nameLabel.setFont(FontManager.getRunescapeFont());
+		nameLabel.setFont(FontManager.getRunescapeBoldFont());
 		nameLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
 
 		JLabel valueLabel = new JLabel(QuantityFormatter.quantityToStackSize(value));
 		valueLabel.setForeground(getColor(value));
-		valueLabel.setFont(FontManager.getRunescapeSmallFont());
+		valueLabel.setFont(FontManager.getRunescapeFont());
 		valueLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
 
 		centerPanel.add(nameLabel);
@@ -112,26 +112,25 @@ public class BankValuerPanel extends PluginPanel
 
 		row.add(centerPanel, BorderLayout.CENTER);
 
-		return row;
+		wrapper.add(row, BorderLayout.CENTER);
+
+		return wrapper;
 	}
 
 	public Color getColor(long value)
 	{
-		if(value < 100_000)
-		{
-			return Color.WHITE;
-		}
-		else if(value < 1_000_000)
+		String valueStr = QuantityFormatter.quantityToStackSize(value);
+		if(valueStr.endsWith("K"))
 		{
 			return Color.YELLOW;
 		}
-		else if(value < 10_000_000)
+		else if(valueStr.endsWith("M"))
 		{
 			return ColorScheme.GRAND_EXCHANGE_PRICE;
 		}
 		else
 		{
-			return Color.RED;
+			return Color.WHITE;
 		}
 	}
 }
